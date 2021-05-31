895b7817d34d88c1b06abbd3b34e877b81795709
paolotrimini <paolo.trimini@gmail.com>
2021-05-31 15:31:42 +0200

    creazione migration

---
+++ MyLaravelonetomany/database/migrations/2021_05_31_132139_add_foreign_keys.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function(Blueprint $table){
+
+            $table -> foreign('employee_id', 'employeetask')
+                   -> references('id')
+                   -> on('employees');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function(Blueprint $table){
+
+            $table -> dropForeign('employeetask');
+
+        });
+    }
+}
